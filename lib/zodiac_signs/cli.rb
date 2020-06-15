1c57197ec3fdd4c5d598a762c94d735eaa94e3a5
Amy Fong <amyhfong@gmail.com>
2020-06-15 15:11:59 +0000

    added ending method

--- lib/zodiac_signs/cli.rb
+++ lib/zodiac_signs/cli.rb
@@ -2,11 +2,23 @@ class ZodiacSigns::CLI
 
   def call
     greeting
+    menu
+    ending
   end
 
   def greeting
     puts "Welcome! Discover your strength and get ready for the day!"
     puts "Please choose your zodiac sign by entering the number from the list below:"
+  end
+
+  def menu(from scraper)
+    puts "list of 12 zodiac signs"
+    input
+
+  end
+
+  def ending
+    puts "Tap into your power and make your day great!"
   end
 
 end
