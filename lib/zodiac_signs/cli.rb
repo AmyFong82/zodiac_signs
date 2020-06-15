98ca80251db9481dbd648daa464e89535ba7ee95
Amy Fong <amyhfong@gmail.com>
2020-06-15 15:41:16 +0000

    manually put in 12 zodiac signs

--- lib/zodiac_signs/cli.rb
+++ lib/zodiac_signs/cli.rb
@@ -8,13 +8,43 @@ class ZodiacSigns::CLI
 
   def greeting
     puts "Welcome! Discover your strength and get ready for the day!"
-    puts "Please choose your zodiac sign by entering the number from the list below:"
   end
 
-  def menu(from scraper)
-    puts "list of 12 zodiac signs"
-    input
-
+  def menu()
+    puts "12 Zodiac Signs"
+    puts "Please enter the number of your zodiac sign from the list above:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.delete "."
+      case input
+      when "1"
+        puts "Aries"
+      when "2"
+        puts "Taurus"
+      when "3"
+        puts "Gemini"
+      when "4"
+        puts "Cancer"
+      when "5"
+        puts "Leo"
+      when "6"
+        puts "Virgo"
+      when "7"
+        puts "Libra"
+      when "8"
+        puts "Scorpio"
+      when "9"
+        puts "Sagittarius"
+      when "10"
+        puts "Capricorn"
+      when "11"
+        puts "Aquarius"
+      when "12"
+        puts "Pisces"
+      else
+        puts "Please enter 1 - 12, or type exit."
+      end
+    end
   end
 
   def ending
